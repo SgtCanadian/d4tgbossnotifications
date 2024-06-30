7247d648540471a96af9e4189d2faf8264e87e26
sgtcanadian <sgtcanadian@hotmail.com>
2024-06-30 09:38:51 -0600

    Fixed typo

--- main.py
+++ main.py
@@ -22,9 +22,9 @@ else:
     config.set("telegram", "chatid", "")
     config.set("telegram", "tokenid", "")
     config.add_section("debug")
-    config.set("debug,","enabled", "")
+    config.set("debug","enabled", "")
 
-if os.getenv("DEBUG_ENABLED", config.get("debug,","enabled")) == True:
+if os.getenv("DEBUG_ENABLED", config.get("debug","enabled")) == True:
     bot = telegram.Bot(token=os.getenv("TELEGRAM_TOKEN", config.get("telegram", "tokenid")))
     asyncio.run(bot.send_message(chat_id=os.getenv("TELEGRAM_CHATID", config.get("telegram", "chatid")), text="Test TG Message"))
 
