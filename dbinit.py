f0acfdcf0ae4efbd441ec794e02b719dafbffada
sgtcanadian <sgtcanadian@hotmail.com>
2024-06-29 20:07:24 -0600

    Cleaned up a bunch of things and made it almost finished.

--- dbinit.py
+++ dbinit.py
@@ -1,87 +1,47 @@
 import sqlite3
 import datetime
+import os
 
 dbname = "bosstimer.db"
 
-
-
-def create_sqlite_database(filename):
-    """ create a database connection to an SQLite database """
-    conn = None
-    try:
-        conn = sqlite3.connect(filename)
-    except sqlite3.Error as e:
-        print(e)
-    finally:
-        if conn:
-            conn.close()
-
-
-def initialize_db(filename):
-
+def initialize_db():
+    #delete the existing db to make from scratch
+    if os.path.exists(dbname):
+        os.remove("bosstimer.db")
+    else:
+        pass
     sql_statements = """CREATE TABLE IF NOT EXISTS bosstimer (
                 time text PRIMARY KEY, 
                 happened boolean
         )"""
     try:
-        with sqlite3.connect(filename) as conn:
+        with sqlite3.connect(dbname) as conn:
             cursor = conn.cursor()
             cursor.execute(sql_statements)
             conn.commit()
     except sqlite3.Error as e:
         print(e)
-
-    #see if the table has been filled already. If not, fill it!
-    cursor.execute("select * from bosstimer")
-    rows = len(cursor.fetchall())
-
+    #initialize last known good time of a boss spawn as anchor point. TODO: make this come from docker env.
     bosstime = datetime.datetime(2024, 6, 26, 0, 0, 0)
-    if rows == 0:               
-        for i in range(6000):
-            timeincrement = datetime.timedelta(minutes=210)
-            bosstime = bosstime + timeincrement
-
-            try:
-
-                cursor.execute("Insert into bosstimer (time,happened) values (?,?)",(bosstime, False))
-            except sqlite3.Error as e:
-                print(e)
-                
-
+    #initialize the next 6000 boss timers, cause fuck it.
+    for i in range(6000):
+        timeincrement = datetime.timedelta(minutes=210)
+        bosstime = bosstime + timeincrement
         try:
-            conn.commit()
+            cursor.execute("Insert into bosstimer (time,happened) values (?,?)",(bosstime, False))
         except sqlite3.Error as e:
             print(e)
-    else:
-        print("Rows already initialized!")
-
-def past(filename):
     try:
-        with sqlite3.connect(filename) as conn:
-            cursor = conn.cursor()
+        conn.commit()
+    except sqlite3.Error as e:
+        print(e)
 
+def past():
+    #this is to change a flag for every boss event to see if it's in the past or future. Since the anchor point is static, this needs to happen.
+    try:
+        with sqlite3.connect(dbname) as conn:
+            cursor = conn.cursor()
             cursor.execute("update bosstimer set happened = 1 where time <= strftime('%Y-%m-%d %H:%M:%S', datetime('now', 'localtime'))")
     except sqlite3.Error as e:
         print(e)
-
-def nexttime(filename):
-    try:
-        with sqlite3.connect(filename) as conn:
-            cursor = conn.cursor()
-
-            cursor.execute("select time from bosstimer where happened = 0 limit 1")
-            time = cursor.fetchone()[0]
-            time = datetime.datetime.strptime(time, '%Y-%m-%d %H:%M:%S')
-    
-            return time
-
-    except sqlite3.Error as e:
-        print(e)
-
-
-if __name__ == '__main__':
-    create_sqlite_database(dbname)
-    initialize_db(dbname)
-    past(dbname)
-    nexttime(dbname)
 
--- main.py
+++ main.py
@@ -3,12 +3,21 @@ import os
 import asyncio
 import schedule
 import time
+import datetime
 from configparser import ConfigParser
 from dbinit import *
+from apscheduler.schedulers.background import BackgroundScheduler
 
+
+#initialize the db from dbinit.py
+initialize_db()
+
+sched = BackgroundScheduler()
+advancewarning = datetime.timedelta(minutes=15)
 config_dir = os.path.normpath(os.getenv("CONFIG_PATH", f"{os.getcwd()}/config"))
 config = ConfigParser()
 config_file = os.path.normpath(f"{config_dir}/config.ini")
+
 if os.path.isfile(config_file):
     config.read(config_file)
 else:
@@ -16,28 +25,43 @@ else:
     config.set("telegram", "chatid", "")
     config.set("telegram", "tokenid", "")
 
-
-
 def message():
     print("nerd")
+    bot = telegram.Bot(token=config.get("telegram","tokenid"))
+    asyncio.run(bot.send_message(chat_id=config.get("telegram","chatid"), text="15 Minute Warning"))
+    
 
+with sqlite3.connect(dbname) as conn:
+    cursor = conn.cursor()
+    past()
+    cursor.execute("select time from bosstimer where happened = 0")
+    cursor.row_factory = None
+    rows = cursor.fetchall()
+    for row in rows:
+        bosstime = datetime.datetime.strptime(row[0], '%Y-%m-%d %H:%M:%S')
+        if bosstime.weekday() >= 0 and bosstime.weekday() <= 4:
+            if bosstime.hour >= 6 and bosstime.hour <= 22:
+                sched.add_job(message, 'date', run_date = bosstime)
+            else:
+                pass
+        else:
+            if bosstime.hour >= 8 and bosstime.hour <= 23:
+                sched.add_job(message, 'date', run_date = bosstime)
+            else:
+                pass
 
+            
 
+sched.start()
 
-def checktime():
-    bosstime = nexttime(dbname)
-    if datetime.datetime.now()
-
-schedule.every(3).seconds.do(checktime)
-
-
-#print(schedule)
 while True:
-   schedule.run_pending()
-   time.sleep(1)
+    time.sleep(5)
+
+
+# Start the scheduler
 
 
 
-#bot = telegram.Bot(token=config.get("telegram","tokenid"))
-#asyncio.run(bot.send_message(chat_id=config.get("telegram","chatid"), text=message))
+
+
 
